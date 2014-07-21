ed56aa5e2486b755d6372e850e79f876edf8ec77
emerdark <emerdark@gmail.com>
2014-07-20 23:10:16 -0400

    Branch 4 takes the lead

--- src/Fourth.java
+++ src/Fourth.java
@@ -5,4 +5,5 @@ public class Fourth {
 	
 	//oh whts that while branch 3 goes to the main stream
 	//here comes branch 4 after it
+	//turns out I like branch 4 better and want to make it the main line, meaning: make every other dev follow this and ammend Branch 3
 }
