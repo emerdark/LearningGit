97aeb7d7a0fa6fe73562828b41d9a250f75a9797
emerdark <emerdark@gmail.com>
2014-07-20 23:14:06 -0400

    Branch 3's last independent commit

--- src/WorkingOnMainLine.java
+++ src/WorkingOnMainLine.java
@@ -3,5 +3,8 @@ public class WorkingOnMainLine {
 	//tada tada tada working on main file
 	//while the branch 4 hides inside my local computer
 	//getting priority stuff done here
+	
+	//seeing that Branch 4 will be accepted as the main line
+	//branch 3 wants to merge to branch 4
 
 }
