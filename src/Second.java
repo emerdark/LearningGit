3f212ce25031c202ddfc550443810a465a45f08d
emerdark <emerdark@gmail.com>
2014-07-20 22:50:06 -0400

    Commiter

--- src/Second.java
+++ src/Second.java
@@ -3,4 +3,13 @@ public class Second {
 	//this is the second class
 	//this would be a branch
 	//changes will be happening to this
+	
+	//Exp1 create branch
+	//keep committing, then push
+	//see what happens
+	
+	
+	void Second1(){
+		System.out.println("Nothing at all");
+	}
 }
