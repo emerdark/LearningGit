ccccefac7f18806114aad7b858457ff1d007035e
emerdark <emerdark@gmail.com>
2014-07-20 23:16:06 -0400

    branched 3 to 4, but in network view Branch 3 seems to be leading, whts going on??

--- src/WorkingOnMainLine.java
+++ src/WorkingOnMainLine.java
@@ -6,5 +6,7 @@ public class WorkingOnMainLine {
 	
 	//seeing that Branch 4 will be accepted as the main line
 	//branch 3 wants to merge to branch 4
+	
+	//branched 3 to 4, but in network view Branch 3 seems to be leading, whts going on??
 
 }
